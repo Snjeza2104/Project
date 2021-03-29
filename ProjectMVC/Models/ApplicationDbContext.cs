f8ec8cda08dd9c61f2d3b7c8f218fd107f26db8f
Snjeza2104 <snjeza.mijosevic@gmail.com>
2021-03-29 13:01:30 +0200

    third commit

---
+++ ProjectMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace ProjectMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            :base("Model1")
+        {
+        }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+       // public virtual DbSet<VehicleMake> VehicleMake { get; set; }
+        //public virtual DbSet<VehicleModel> VehicleModel { get; set; }
+    }
+}
